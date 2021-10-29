63846daeb17e32b0a767483418a4c5cc7d488c2e
china-srun <91663484+china-srun@users.noreply.github.com>
2021-10-29 21:59:56 +0700

    Update Bootcamp_week1 (2020).js

--- Bootcamp_week1 (2020).js
+++ Bootcamp_week1 (2020).js
@@ -6,6 +6,7 @@
 
 
 // Exercise 03
+//console.log("BOOTCAMP RULES:\n#1 Before getting started read the instructions TWICE!\n#2 IF you have any QUESTIONS... ASK!\n#3 When you create a file: DOUBLE check the filename.\n#4 When you are done with a program: CHECK AND TEST IT AGAIN!\n#5 IF one CHARACTER is different from instructions: YOU FAIL!")
 
 
 // Exercise 04
